b17a02a461c96cbda4ae744a14b9ecd8c8734a8c
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2023-08-26 05:45:38 +1000

    I think it is better to def function in the main body and run them inside main, this is better for debugging.
    New commit:
    Added model.py (for initializing game and player model) and responses.py
    
    Please separate async functions (which deals with IO) from regular (computational ones)

--- Werebot-main.py
+++ Werebot-main.py
@@ -3,6 +3,8 @@ import os
 
 import discord
 from dotenv import load_dotenv
+import responses
+import model
 
 # Define all our constants here (and only constants!)
 
@@ -12,6 +14,16 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client(intents=discord.Intents.default())  # can this go into run_werebot function?
+
+"""
+It is best to separate computational functions (with regular def) from async functions, then let the async functions
+call the regular functions.
+regular function goes below:
+"""
+
+"""
+Async functions:
+"""
 
 
 @client.event  # probably put in run_werebot?
@@ -40,7 +52,7 @@ def run_werebot():
     pass
 
 
-def main():
+async def main():
     pass
 
 
