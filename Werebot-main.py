46377d208dccb64fb6fa3da0b6a1e5188edf34ef
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2023-08-26 09:19:19 +1000

    Attempt to run bot

--- Werebot-main.py
+++ Werebot-main.py
@@ -74,6 +74,15 @@ async def on_ready():
     # print(f'Guild Members:\n - {members}')
 
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'hello':
+        await message.channel.send('Hi~')
+
+
 client.run(TOKEN)  # I think this should also go inside run_werebot
 
 
