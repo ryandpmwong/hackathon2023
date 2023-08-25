5e387c98c6c1b3e4c758ad682ab61b08c80031a0
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2023-08-26 08:40:18 +1000

    Modified the model.py, Added WereWolfGame class

--- Werebot-main.py
+++ Werebot-main.py
@@ -2,6 +2,8 @@
 import os
 
 import discord
+from discord import app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
 import responses
 import model
@@ -15,11 +17,38 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client(intents=discord.Intents.default())  # can this go into run_werebot function?
 
+
+class WereWolfGame(commands.Cog):
+    def __init__(self, bot, players: list, game_mode='text'):
+        self.bot = bot
+        self.player = players
+        self.game_model = model.GameModel(len(players), game_mode)
+
+    @app_commands.command()  #apparently this will enable to show us command in discord
+    @app_commands.describe(
+        number_of_people="The number of people in the game",
+        game_mode="Game mode, can be voice or text"
+    )
+    async def werewolfgame(self, interation: discord.Interaction, number_of_people: int = 5, game_mode: str = 'text'):
+        """
+        Starts a game of werewolf
+        :param interation:
+        :param number_of_people:
+        :param game_mode:
+        :return:
+        """
+
+
 """
 It is best to separate computational functions (with regular def) from async functions, then let the async functions
 call the regular functions.
 regular function goes below:
 """
+
+
+
+
+
 
 """
 Async functions:
--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import math
+
+
 class GameModel:
     """
     Initializes a new game of Werewolf with appropriate number of players
@@ -9,18 +11,24 @@ class GameModel:
     Also, what should happen if not enough people sign up?
     """
 
-    def __init__(self, num_players: int, game_mode='text'):
+    def __init__(self, num_players: int, game_mode):
+        # In class Game of Werebot-main, the game mode is defined to be "text" by default. Perhaps doing game mode in
+        # class Game is better, so we can have game model only focus on internal stuff - to be discussed
+
         self.num_players = num_players
         self.game_mode = game_mode
-        villager_num = math.ceil(num_players / 3.5)
-        werewolf_num = num_players - villager_num
-        self.roles = {'villager' : villager_num, 'werewolf' : werewolf_num})
-        self.players = generate_players(num_players)
+        self.villager_num = math.ceil(num_players / 3.5)
+        self.werewolf_num = num_players - self.villager_num
+        self.roles = {'villager': self.villager_num, 'werewolf': self.werewolf_num}
+        self.user_list = []  # list of player objects
+        # need to generate user list from methods
+        self.players = self.generate_players(num_players)
+        self.day = 0
 
-    def generate_players(self, amount):
-        for i in range(0,amount):
-            
 
+    def generate_players(self, user_list):
+        for user in self.user_list:
+            pass
 
 
 class Player:
@@ -29,6 +37,7 @@ class Player:
     Subclasses: Werewolf, Village, Witch, Hunter....
     Incomplete. Need to add more functionality
     """
+
     def __init__(self, username):
         self.username = username
         self.alive = True
@@ -40,8 +49,14 @@ class Player:
     def get_side(self):
         return None  # might modify later: eg. by default return good
 
+    def get_role(self):
+        return "Player"
+
     def is_alive(self):
         return self.alive
+
+    def kill(self):
+        self.alive = False
 
 
 class Werewolf(Player):
@@ -49,7 +64,10 @@ class Werewolf(Player):
         super().__init__(username)
 
     def get_side(self):
-        return 'Werewolf'
+        return 'Bad'
+
+    def get_role(self):
+        return "Werewolf"
 
 
 class Villager(Player):
@@ -57,18 +75,50 @@ class Villager(Player):
         super().__init__(username)
 
     def get_side(self):
-        return 'Villager'
+        return 'Good'
+
+    def get_role(self):
+        return "Villager"
 
 
-class Witch(Player):
+class Witch(Player):  # or doctor? Whatever name you guys want
     """
     Witch owns a poison and a cure medicine.
     They can choose to save the people being killed or poison others at night
     """
+
     def __init__(self, username):
         super().__init__(username)
-        self.potions = ['poison', 'medicine']
+        self.poison = True
+        self.medicine = True
 
     def get_side(self):
         return 'Good'
 
+    def get_role(self):
+        return "Witch"
+
+    def apply_poison(self, other: Player) -> bool:
+        """
+        :param other: The player to apply poison on
+        :return: True if the application is successful, False otherwise
+        """
+        if self.poison is True and other.is_alive():
+            other.kill()
+            self.poison = False
+            return True
+        return False
+
+    def apply_medicine(self, other: Player) -> bool:
+        """
+        :param other: The player to apply poison on
+        :return: True if the application is successful, False otherwise
+        """
+        if self.medicine is True and other.is_alive():
+            other.kill()
+            self.medicine = False
+            return True
+        return False
+
+    def get_potion_status(self):
+        return f"poison: {int(self.poison)}, medicine: {int(self.medicine)}"
