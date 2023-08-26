b53950bcb9a9e116604dde5bc47aa409d46e4207
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2023-08-27 00:17:57 +1000

    Merged / commands under a mega-entity, /play
    Red bills

--- promptView2.py
+++ promptView2.py
@@ -48,11 +48,14 @@ async def on_ready():
 ########## I'm really sorry I don't know how to use classes and can't run this with them ############
 
 @bot.command()
-async def test_play(ctx, arg1 = None, arg2 = None):
-    """Runs on /test_play"""
-    if arg1.lower() in ["h", "help"]:
+async def play(ctx, arg1 = None, arg2 = None):
+    """Runs on /test_play [num players] [num werewolf]"""
+    if arg1 != None and arg1.lower() in ["h", "help"]:
         await ctx.send("Usage: /test_play [total number of players] [number of werewolves]")
         return
+    
+    nickname = ctx.author.nick
+    username = ctx.author
         
     async def button_join_callback(interaction):
         """Prints username/nicknames and a message"""
@@ -65,13 +68,13 @@ async def test_play(ctx, arg1 = None, arg2 = None):
         #players_joined += 1
         #print(players_joined)
         message = await interaction.original_response()
-        await message.edit(f"New or old message?")
+        await message.edit()
 
     async def button_start_callback(interaction):
         message = await interaction.original_response()
         print(message)
         await interaction.edit_original_response("New or old message first method?")
-        await message.edit("New or old message?")
+        await message.edit()
 
     async def button_boring_callback(interaction):
         await interaction.response.send_message("Oh. You clicked the other button.")
@@ -96,14 +99,43 @@ async def test_play(ctx, arg1 = None, arg2 = None):
             view.add_item(button_start)
             view.add_item(button3)
             #view.remove_item(button)
-            start_message = await ctx.send((f"{ctx.author.nick} ({ctx.author}) has started a {arg2} werewolf game! 🐺" \
-                                            f"\n {players_joined}/{arg1} players joined"),
-                                            view=view)
+
+            if nickname != None:
+                start_message = await ctx.send((f"{nickname} ({username}) has started a {arg2} werewolf game! 🐺" \
+                                                f"\n {players_joined}/{arg1} players joined"),
+                                                view=view)
+            else:
+                start_message = await ctx.send((f"{username} has started a {arg2} werewolf game! 🐺" \
+                                f"\n {players_joined}/{arg1} players joined"),
+                                view=view)
         else:
-           await ctx.send("Too many werewolves.")
+           await ctx.send("Too many werewolves. Please enter a lower amount of werewolves.")
 
     elif arg1:
-        await ctx.send(str(ctx.author)+" wants to start a game with "+arg1+" players.")
+
+        players_joined = 0
+
+        #await ctx.send(str(ctx.author)+" wants to start a game with "+arg1+" players and "+arg2+" werewolves.")
+
+        button_join.callback = button_join_callback
+        button_start.callback = button_start_callback
+        button3.callback = button_boring_callback
+
+        view = View()
+        view.add_item(button_join)
+        view.add_item(button_start)
+        view.add_item(button3)
+        #view.remove_item(button)
+
+        if nickname != None:
+            start_message = await ctx.send((f"{nickname} ({username}) has started a game! 🐺" \
+                                    f"\n {players_joined}/{arg1} players joined"),
+                                    view=view)
+        else:
+            start_message = await ctx.send((f"{username} has started a game! 🐺" \
+                        f"\n {players_joined}/{arg1} players joined"),
+                        view=view)
+        #await ctx.send(str(ctx.author)+" wants to start a game with "+arg1+" players.")
     else:
         await ctx.send("You haven't entered the number of players.")
     
@@ -114,47 +146,6 @@ def valid_player_numbers(total, werewolves):
         return False
     else:
         return True
-
-
-@bot.command()
-# Make Button?
-async def play(ctx):
-    button_join = Button(label="Join Game", style=discord.ButtonStyle.blurple)
-    button_start = Button(label="Start Game", style=discord.ButtonStyle.green, disabled=True)
-    button3 = Button(label="Warning: I'm broken, Cancel", style=discord.ButtonStyle.red)
-    players_joined = 0
-
-    async def button_join_callback(interaction):
-        nickname = interaction.user.nick
-        username = interaction.user
-        if nickname == None:
-            await interaction.response.send_message(f"{username} has clicked a button! Blasphemous!")
-        else:
-            await interaction.response.send_message(f"{nickname} ({username}) has clicked a button! Blasphemous!")
-        #players_joined += 1
-        #print(players_joined)
-        message = await interaction.original_response()
-        await message.edit(f"New or old message?")
-
-    async def button_start_callback(interaction):
-        message = await interaction.original_response()
-        print(message)
-        await interaction.edit_original_response("New or old message first method?")
-        await message.edit("New or old message?")
-
-    async def button_boring_callback(interaction):
-        await interaction.response.send_message("Oh. You clicked the other button.")
-
-    button_join.callback = button_join_callback
-    button_start.callback = button_start_callback
-    button3.callback = button_boring_callback
-
-    view = View()
-    view.add_item(button_join)
-    view.add_item(button_start)
-    view.add_item(button3)
-    #view.remove_item(button)
-    start_message = await ctx.send(f"Username of some sort has started a game! 🐺\n {players_joined}/666 players joined", view=view)
 
 bot.run(TOKEN)
 
