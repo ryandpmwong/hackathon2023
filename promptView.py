c3c0149237ca278abdd5045e0fb7f67b58bae8fc
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2023-08-26 17:35:32 +1000

    Bot will now hate on you if click one (1) buttons!

--- promptView.py
+++ promptView.py
@@ -13,13 +13,28 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
+class MakeButtons:
+
+    def __init__(self):
+        self.all_players_thread = None
+        self.werewolves_thread = None
+        self.dead_thread = None
+
+    async def create_start_prompt(self):
+        return
+
 #@tree.command(name = "hello", description = "My first application Command", guild=discord.Object(id=1144540988753326131))
 
 #async def first_command(interaction):
 #    await interaction.response.send_message("Hello!")
 
 bot = commands.Bot(command_prefix="/",
-                        intents=discord.Intents.all())
+                   intents=discord.Intents.all())
+
+"""@bot.command()
+# Make Button?
+async def hello(ctx):
+    classMaybe = """
 
 
 @client.event
@@ -30,6 +45,8 @@ async def on_ready():
 async def on_ready():
 #    await tree.sync(guild=discord.Object(id=1144540988753326131))
     print("Ready!")
+
+########## I'm really sorry I don't know how to use classes and can't run this with them ############
 
 # make the slash command
 @tree.command(name="name", description="description")
@@ -42,6 +59,12 @@ async def hello(ctx):
     button1 = Button(label="Don't click me ;_;", style=discord.ButtonStyle.blurple)
     button2 = Button(label="I would ask you to start the game.", style=discord.ButtonStyle.green)
     button3 = Button(label="Warning: I'm broken", style=discord.ButtonStyle.red)
+
+    async def button1_callback(interaction):
+        await interaction.response.send_message("You're mean ._.")
+
+    button1.callback = button1_callback
+
     view = View()
     view.add_item(button1)
     view.add_item(button2)
