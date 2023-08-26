4c6f1b2d39587b1b35ee6b94dab968f761b4328d
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2023-08-26 16:54:03 +1000

    These buttons may look fancier, but it's no more functional than it was.

--- promptView.py
+++ promptView.py
@@ -1,25 +1,55 @@
 import discord
 from discord.ui import Button, View
 from discord.ext import commands
+import discord.ext
+#from discord import app_commands
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-botThing = commands.Bot(command_prefix="/",
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+#@tree.command(name = "hello", description = "My first application Command", guild=discord.Object(id=1144540988753326131))
+
+#async def first_command(interaction):
+#    await interaction.response.send_message("Hello!")
+
+bot = commands.Bot(command_prefix="/",
                         intents=discord.Intents.all())
 
-@botThing.command()
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=1144540988753326131))
+    # print "ready" in the console when the bot is ready to work
+    print("ready")
+async def on_ready():
+#    await tree.sync(guild=discord.Object(id=1144540988753326131))
+    print("Ready!")
+
+# make the slash command
+@tree.command(name="name", description="description")
+async def slash_command(interaction: discord.Interaction):    
+    await interaction.response.send_message("command")
+
+@bot.command()
 # Make Button?
 async def hello(ctx):
-    button = Button(label="Don't click me ;_;", style=discord.ButtonStyle.blurple)
+    button1 = Button(label="Don't click me ;_;", style=discord.ButtonStyle.blurple)
+    button2 = Button(label="I would ask you to start the game.", style=discord.ButtonStyle.green)
+    button3 = Button(label="Warning: I'm broken", style=discord.ButtonStyle.red)
     view = View()
-    view.add_item(button)
+    view.add_item(button1)
+    view.add_item(button2)
+    view.add_item(button3)
     #view.remove_item(button)
-    await ctx.send("Hi", view=view)
+    await ctx.send("Username of some sort has started a game! 🐺\n x/y players joined", view=view)
 
-botThing.run(TOKEN)
+bot.run(TOKEN)
 
 """class ButtonView(discord.ui.View):
     def __init__(self):
