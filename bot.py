45d47f031dca422ee2552f5aabdf6ee36c3fbc3c
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2023-08-26 13:24:02 +1000

    Fixed bot talking to each other issue

--- bot.py
+++ bot.py
@@ -17,17 +17,17 @@ class WereWolfBot(commands.Bot):
         print("Ready")
 
     async def on_message(self, message):
-        if message.author == self.user:
+        if message.author == self.user or message.author.bot:
             return
 
         if message.content == "Hi":
             await message.channel.send("Hello~")
         print(message)
-        if message.channel.name == 'testing':
+        if message.channel.name == 'testing' or message.author.bot:
             await message.channel.send(f"{message.author} has send a message: {message.content}")
 
     async def on_message_edit(self, before, after):
-        if before.channel.name != 'testing':
+        if before.channel.name != 'testing' or before.author.bot:
             return
         await before.channel.send(
             f"{before.author} edited a message\n"
