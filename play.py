7a7e8528d2c4010278ed45146b6035a941c0e14f
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2023-08-26 12:00:09 +1000

    main-test.py for testing your bot.

---
+++ play.py
@@ -0,0 +1,16 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import bot
+
+class Play(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="play")
+    async def play_game(self, interaction: discord.Interaction):
+        print("testing 123")
+
+async def setup(bot: bot.WereWolfBot):
+    await bot.add_cog(Play(bot))
+
