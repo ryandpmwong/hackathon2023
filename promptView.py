2e18f65bb4d2b6f361ab8625ba0eac12b1373604
InsomniacCrow <143170718+InsomniacCrow@users.noreply.github.com>
2023-08-26 16:23:03 +1000

    I made a button. What does it do? *absolutely nothing*

--- promptView.py
+++ promptView.py
@@ -1,10 +1,30 @@
 import discord
-import asyncio
+from discord.ui import Button, View
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
 
-class ButtonView(discord.ui.View):
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+botThing = commands.Bot(command_prefix="/",
+                        intents=discord.Intents.all())
+
+@botThing.command()
+# Make Button?
+async def hello(ctx):
+    button = Button(label="Don't click me ;_;", style=discord.ButtonStyle.blurple)
+    view = View()
+    view.add_item(button)
+    #view.remove_item(button)
+    await ctx.send("Hi", view=view)
+
+botThing.run(TOKEN)
+
+"""class ButtonView(discord.ui.View):
     def __init__(self):
         super().__init__()
 
     @discord.ui.button(label="what is on the button", style=discord.ButtonStyle.gray)
     async def function_name(self, int: discord.Interaction):
-        await int.response.send_message("button clicked")
+        await int.response.send_message("button clicked")"""
